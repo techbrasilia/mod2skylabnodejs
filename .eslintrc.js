1b46c759780be974cba538cc2be2a04ff26063bf
techbrasilia <techbrasilia@outlook.com>
2019-10-21 23:38:31 -0300

    alteração jwt

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,26 +3,24 @@ module.exports = {
     es6: true,
     node: true,
   },
-  extends: [
-    'airbnb-base',
-    'prettier'
-  ],
-  
-  plugins: ['prettier'],
-
+  extends: ['airbnb-base', 'prettier'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
   },
   parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
     ecmaVersion: 2018,
     sourceType: 'module',
   },
+  plugins: ['prettier'],
   rules: {
-    "prettier/prettier": "error",
-    "class-methods-use-this": "off",
-    "no-param-reassign": "off",
-    "camelcase": "off",
-    "no-unused-vars": ["error", { "argsIgnorePattern": "next" }]
+    'prettier/prettier': 'error',
+    'class-methods-use-this': 'off',
+    'no-param-reassign': 'off',
+    camelcase: 'off',
+    'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
   },
 };
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'aad8c0f4cc7b27f7d72d080d1574c1ba',
+  expiresIn: '7d',
+};
