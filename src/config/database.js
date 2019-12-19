b26b6dda09578474d05c2073cea37411dc1eca19
techbrasilia <techbrasilia@outlook.com>
2019-12-19 14:41:28 -0300

    File do avatar, relacionamento com user e lista de providers

--- src/config/database.js
+++ src/config/database.js
@@ -2,8 +2,8 @@ module.exports = {
   dialect: 'postgres',
   host: 'localhost',
   username: 'postgres',
-  password: 'docker',
-  database: 'gobarber',
+  password: 'postgres',
+  database: 'gonodemodulo2',
   define: {
     timestamp: true,
     underscored: true,
--- src/database/index.js
+++ src/database/index.js
@@ -1,10 +1,11 @@
 import Sequelize from 'sequelize';
 
 import User from '../app/models/User';
+import File from '../app/models/File';
 
 import databaseConfig from '../config/database';
 
-const models = [User];
+const models = [User, File];
 
 class Database {
   constructor() {
@@ -12,9 +13,11 @@ class Database {
   }
 
   init() {
-    this.connetion = new Sequelize(databaseConfig);
+    this.connection = new Sequelize(databaseConfig);
 
-    models.map(model => model.init(this.connetion));
+    models
+      .map(model => model.init(this.connection))
+      .map(model => model.associate && model.associate(this.connection.models));
   }
 }
 
